d7ae0e35c8a3eb84f4b116f51d4c94fc40f92fca
Sergio Mallea <sergiomallea80@gmail.com>
2022-09-11 17:30:18 -0400

    Added Tasks for Lists

---
+++ tasks/ListTasks/task1.py
@@ -0,0 +1,19 @@
+"""
+Many organizations have user ids which are constrained in some way. 
+Imagine you work at an internet service provider and the user ids are all two letters followed by two numbers (e.g. aa49). 
+Your task at such an organization might be to hold a record on the billing activity for each possible user. 
+Write an initialization line as a single list comprehension which creates a list of all possible user ids. 
+Assume the letters are all lower case.
+"""
+
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = []
+answer = [(firstLetter  + secondLetter  + firstDigit + seconDigit) for firstLetter in lowercase  for secondLetter in lowercase  for firstDigit in digits for seconDigit in digits]
+print(answer)
+
+first_10 = ['aa00', 'aa01', 'aa02', 'aa03', 'aa04', 'aa05', 'aa06', 'aa07', 'aa08', 'aa09']
+last_10 =  ['zz90', 'zz91', 'zz92', 'zz93', 'zz94', 'zz95', 'zz96', 'zz97', 'zz98', 'zz99']
+assert answer[:10] == first_10
+assert answer[-10::] == last_10
